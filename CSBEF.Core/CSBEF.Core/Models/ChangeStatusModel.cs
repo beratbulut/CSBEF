490bb6e1d911e3482ba9e82931d5b703d0e510b2
Mesut Kurak <mesutkurak@hotmail.com>
2020-01-05 16:00:10 +0300

    2.2.2

--- CSBEF.Core/CSBEF.Core/Models/ChangeStatusModel.cs
+++ CSBEF.Core/CSBEF.Core/Models/ChangeStatusModel.cs
@@ -1,7 +1,11 @@
-﻿namespace CSBEF.Core.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSBEF.Core.Models
 {
-    public class ChangeStatusModel
+    public class ChangeStatusModel : HashControlModel
     {
+        [Required(ErrorMessage = "ModelValidationError_IdRequired")]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "ModelValidationError_IdIsZero")]
         public int Id { get; set; }
         public bool Status { get; set; }
     }
