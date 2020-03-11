db02a19c2b6c12542e8fb574c7dbb5833e03f4c9
Mesut Kurak <mesutkurak@hotmail.com>
2020-03-11 16:32:13 +0300

    2.2.9

--- CSBEF.Core/CSBEF.Core/Abstracts/ServiceBase.cs
+++ CSBEF.Core/CSBEF.Core/Abstracts/ServiceBase.cs
@@ -33,7 +33,7 @@ namespace CSBEF.Core.Abstracts
         protected ILogger<ILog> _logger;
         protected IMapper _mapper;
         protected IEventService _eventService;
-        private readonly IHubSyncDataService _hubSyncDataService;
+        protected readonly IHubSyncDataService _hubSyncDataService;
 
         #endregion Dependencies
 
